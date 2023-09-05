242dffd0a3d1db08367dbb522fe893d1ca678919
Luis Ricardo <luiscastrodev@gmail.com>
2023-09-05 08:36:05 -0300

    add Dao

---
+++ app/src/main/java/hoods/com/jetexpense/data/local/Dao.kt
@@ -0,0 +1,52 @@
+package hoods.com.jetexpense.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import androidx.room.Update
+import hoods.com.jetexpense.data.local.models.Expense
+import hoods.com.jetexpense.data.local.models.Income
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ExpenseDao {
+    @Insert(onConflict = REPLACE)
+    suspend fun insertExpense(expense: Expense)
+
+    @Query("DELETE FROM  expense_table")
+    suspend fun deleteAllExpense()
+
+    @Query("DELETE FROM expense_table WHERE expense_id = :id")
+    suspend fun deleteExpense(id: Int): Int
+
+    @Update(onConflict = REPLACE)
+    suspend fun updateExpense(expense: Expense)
+
+    @Query("SELECT * FROM expense_table ")
+    fun getAllExpense(): Flow<List<Expense>>
+
+    @Query("SELECT * FROM expense_table WHERE expense_id = :id")
+    fun getExpenseById(id: Int): Flow<Expense>
+}
+
+@Dao
+interface IncomeDao {
+    @Insert(onConflict = REPLACE)
+    suspend fun insertIncome(income: Income)
+
+    @Query("DELETE FROM  income_table")
+    suspend fun deleteAllIncome()
+
+    @Query("DELETE FROM income_table WHERE income_id = :id")
+    suspend fun deleteIncome(id: Int): Int
+
+    @Update(onConflict = REPLACE)
+    suspend fun updateIncome(income: Income)
+
+    @Query("SELECT * FROM income_table ")
+    fun getAllIncome(): Flow<List<Income>>
+
+    @Query("SELECT * FROM income_table WHERE income_id = :id")
+    fun getIncomeById(id: Int): Flow<Income>
+}
